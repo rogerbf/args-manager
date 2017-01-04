0c0a50fba62a28cad3d92314c6df5ae7c194b9f9
rogerbf <roger@bergfalk.nu>
2017-01-04 18:39:05 +0100

    remove commented code

--- source/library/validate.js
+++ source/library/validate.js
@@ -1,20 +1,5 @@
 import normalize from './normalize'
 
-// export default (validArgs, arg) => {
-//   if (validArgs) {
-//     const normalized = normalize(arg)
-//     const included = normalized.filter(key => validArgs.includes(key))
-//
-//     if (normalized.length !== included.length) {
-//       throw new Error(`argument could not be found in valid arguments array`)
-//     } else {
-//       return arg
-//     }
-//   } else {
-//     return arg
-//   }
-// }
-
 export default (validArgs, arg) => {
   if (validArgs) {
     const normalized = normalize(arg)
